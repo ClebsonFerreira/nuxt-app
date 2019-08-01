493bb0cba00a7383095c88c64c3afe46a2d9f78a
clebson ferreira da silva <clebson.silva@linx.com.br>
2019-08-01 11:04:44 -0300

    consultando um webservices com axios

--- pages/about.vue
+++ pages/about.vue
@@ -11,7 +11,41 @@
       <h2 class="subtitle">
        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias quis accusantium aut sequi aliquam! Doloremque commodi quidem ipsam deserunt odit numquam mollitia ipsa animi dignissimos veritatis, illum inventore nemo dolor?
       </h2>
+      <h2 class="subtitle">
+       Participantes
+      </h2>
+      <ul>
+        <li v-for="list of user" :key="list.id">
+          {{list.name}}
+          <ul>
+            <li> Email : {{list.email}}</li>
+            <li> Telefone : {{list.phone}}</li>
+          </ul>
+          <br>
+        </li>
+      </ul>
     </div>
   </div>
 </section>
 </template>
+
+<script>
+  export default {
+    name:'about',
+    data: () => {
+    return {
+      user:{}
+    }
+  },
+    methods:{
+    },
+    async mounted () {
+      const user  = await this.$axios.$get('https://jsonplaceholder.typicode.com/users');
+      this.user = user;
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
